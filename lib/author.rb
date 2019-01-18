da9ae99739eeaaddc87fcd065dcf0476259331c7
Ron Lynch <lynch.ronald1@gmail.com>
2019-01-18 04:13:50 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,20 @@
+class Author
+  attr_accessor :name, :post
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post
+
+  end
+
+  def add_post_by_title
+
+  end
+end
